95edc89c06668348dd716107e8dd92f410d3971b
jsanchez.net <jsanchezsilvera@gmail.com>
2022-04-03 13:20:27 -0300

    removed tests from implementation file

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# jsanchez_dynamic_array.h
+# jsanchez_dynamic_array
 Generic dynamic array implementation in C
+
+:information_source: this only compiles/works in C.
+
+# How to use
+
+- include jsanchez_dynamic_array.h in your project
+``` #include "jsanchez_dynamic_array.h" ```
+
+```
+// Create a pointer with the desired array type
+my_struct *StructArray;
+
+// Init the array, this allocates size for the array metadata and sets the array size and capacity to 0
+ArrayInit(StructArray);
+
+// Push to the end of the array, the array grows dynamically and each time the capacity is reached it allocates the double of the space used and copies everything onto the new memory pool
+my_struct Foo;
+ArrayPush(StructArray, Foo);
+
+// Pops the last element of the array. The array also resized down when the size is 1/3 of the capacity and you pop one element.
+my_struct *Foo;
+ArrayPop(StructArray, &Foo);
+
+// Get the current array size and capacity, ArrayCount is the same as ArraySize, i prefer ArrayCount but Size might be clearer
+int Size = ArraySize(StructArray);
+int Capacity = ArrayCapacity(StructArray);
+
+// Free the memory and metadata
+ArrayFree(StructArray);
+
+# How to run tests
+
+# License
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
---
+++ jsanchez_dynamic_array.h
@@ -0,0 +1,87 @@
+#pragma once
+
+// NOTE(Jorge): This only compiles in C
+
+#include <stdlib.h>
+#include <assert.h>
+
+#define ArrayCount(ARR) (*((int *) ARR - 2))
+#define ArraySize(ARR) (*((int *) ARR - 2))
+#define ArrayCapacity(ARR) (*((int *) ARR - 1))
+
+// Allocates space for the array metadata and sets the Size and Capacity to 0.
+#define ArrayInit(ARRAY)                        \
+    do {                                        \
+        int *Raw = malloc(2 * sizeof(int));     \
+        assert(Raw);                            \
+        Raw[0] = 0;                             \
+        Raw[1] = 0;                             \
+        ARRAY = (void*)&Raw[2];                 \
+    } while(0)                                  \
+
+// 1- Check if ARRAY is valid
+// 2- Set Raw Pointer
+// 3- Free
+// 4- Set ARRAY to NULL
+#define ArrayFree(ARRAY)                        \
+    do {                                        \
+        assert(ARRAY);                          \
+        int *Raw = ((int*)(ARRAY) - 2);         \
+        free(Raw);                              \
+        ARRAY = NULL;                           \
+    } while(0)                                  \
+
+// 1- Set Raw ptr, Raw[0] == ArraySize | Raw[1] == ArrayCapacity
+// 2- ArraySize + 1
+#define ArrayPush(ARRAY, VALUE)                                         \
+    do {                                                                \
+        int *Raw = ((int*)(ARRAY) - 2);                                 \
+        Raw[0] = Raw[0] + 1;                                            \
+        if(Raw[1] == 0)                                                 \
+        {                                                               \
+            Raw[1] = 1;                                                 \
+            Raw = realloc(Raw, 2 * sizeof(int) + Raw[1] * sizeof((VALUE))); \
+            assert(Raw);                                                \
+            (ARRAY) = (void*)&Raw[2];                                   \
+        }                                                               \
+        if(Raw[0] > Raw[1])                                             \
+        {                                                               \
+            Raw[1] = 2 * Raw[1];                                        \
+            Raw = realloc(Raw, 2 * sizeof(int) + Raw[1] * sizeof((VALUE))); \
+            assert(Raw);                                                \
+            (ARRAY) = (void*)&Raw[2];                                   \
+        }                                                               \
+        ARRAY[Raw[0] - 1] = (VALUE);                                    \
+    } while(0)                                                            \
+
+
+// 1- Verify ARRAY is valid
+// 2- Verify OUT is valid
+// 3- Set Raw pointer, Raw[0] == Size | Raw[1] == Capacity
+// 4- Make sure we are not trying to pop an empty array
+// 5- If Array Size > 0, set OUT to the last items in the array
+// 6- Decrement Array Size by 1
+// 7- If ArraySize <= ArrayCapacity/3 realloc and copy
+// 8- If ArraySize <= 1/4 of ArrayCapacity, realloc to be ArraySize * 2 * sizeof(Type) + Raw
+// TODO(Jorge): Popping from an empty array should not crash!
+#define ArrayPop(ARRAY, OUT)                                        \
+    do  {                                                           \
+        assert(ARRAY);                                              \
+        assert(OUT);                                                \
+        int *Raw = ((int*)(ARRAY) - 2);                             \
+        if(Raw[0] == 0)                                             \
+        {                                                           \
+            assert(0 && "Array Size is 0, cannot pop the array");   \
+        }                                                           \
+        if(Raw[0] > 0)                                              \
+        {                                                           \
+            (*(OUT)) = ARRAY[Raw[0] - 1];                           \
+            Raw[0] -= 1;                                            \
+                                                                    \
+            if(Raw[0] <= Raw[1]/4)                                  \
+            {                                                       \
+                Raw = realloc(Raw, 2 * sizeof(int) + Raw[0] * 2 * sizeof(*(OUT))); \
+                assert(Raw);                                            \
+            }                                                       \
+        }                                                           \
+    } while(0)                                                      \
